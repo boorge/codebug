e8dd4d770e6b57c3c75ebdfef57b7925403d0e43
Kiran Borge <borgekiran123@gmail.com>
2022-06-17 10:16:08 -0700

    codebug

---
+++ Courses Details/JavaScript/index.js
@@ -0,0 +1,7 @@
+
+// sticky navigation
+
+window.addEventListener("scroll", function () {
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
